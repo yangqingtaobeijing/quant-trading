870cf02f0b6a2f2fe263c0cfb657c514f12d6686
oeyang <yangqingtaowx@gmail.com>
2026-04-27 19:49:01 +0800

    chore: add railway deploy config and env var support

--- quant-backend/main.py
+++ quant-backend/main.py
@@ -46,13 +46,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "http://localhost:5176",
-        "http://localhost:5177",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
